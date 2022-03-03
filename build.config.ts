2b77397127152baa0aa2d391ef0124fe991f1e32
xmsz <137819866@qq.com>
2022-03-03 16:47:56 +0800

    feat: @unocss/transformer-directives

--- build.config.ts
+++ build.config.ts
@@ -1,10 +1,12 @@
 import Unocss from 'unocss/vite';
 import { presetIcons, presetUno } from 'unocss';
+// import transformerDirective from '@unocss/transformer-directives';
 
 export default {
   vite: true,
   vitePlugins: [
     Unocss({
+      // transformers: [transformerDirective()],
       presets: [presetUno(), presetIcons({})],
     }),
   ],
