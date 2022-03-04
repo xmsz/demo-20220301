5815d061539c5616cb49d0fe45b2e25e90843b6d
xmsz <137819866@qq.com>
2022-03-04 10:40:26 +0800

    chore: 升级办吧

--- build.config.ts
+++ build.config.ts
@@ -1,12 +1,12 @@
 import Unocss from 'unocss/vite';
 import { presetIcons, presetUno } from 'unocss';
-// import transformerDirective from '@unocss/transformer-directives';
+import transformerDirective from '@unocss/transformer-directives';
 
 export default {
   vite: true,
   vitePlugins: [
     Unocss({
-      // transformers: [transformerDirective()],
+      transformers: [transformerDirective()],
       presets: [presetUno(), presetIcons({})],
     }),
   ],
